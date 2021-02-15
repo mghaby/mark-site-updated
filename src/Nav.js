db9328f34d52ccc51f1d9937ef2d08e847508a44
mghaby <mark.ghaby71@hotmail.com>
2021-02-16 00:02:23 +1100

    fixed router localhost issue

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,8 @@
 import {
   BrowserRouter as Router,
   Switch,
-  Route} from "react-router-dom";
+  Route,
+  Link} from "react-router-dom";
 import Projects from './Projects';
 import AcaHistory from './AcaHistory';
 import ContactMe from './ContactMe';
@@ -11,22 +12,7 @@ import App from './App'
 
 function Nav() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <App />
-          </Route>
-          <Route path="/Projects">
-            <Projects />
-          </Route>
-          <Route path="/Academic">
-            <AcaHistory />
-          </Route>
-          <Route path="/Contact">
-            <ContactMe />
-          </Route>
-          </Switch>
-      </Router>
+      <h1>Nav</h1>
     );
   }
 
