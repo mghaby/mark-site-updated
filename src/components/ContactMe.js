e1646d3736f8452dc92cdba12d95794491683a51
mghaby <mark.ghaby71@hotmail.com>
2021-02-17 12:41:45 +1100

    installed material ui and added some elements and styling to contactMe component

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,9 +1,56 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function ContactMe() {
+  const classes = useStyles();
     return (
       <div>
-        <h1>ContactMe</h1>
+        <h1>Contact Me!</h1>
+        <form>
+        <Grid
+            container
+            direction="column"
+            justify="space-between"
+            alignItems="center"
+          >
+
+        <TextField
+          required
+          id="Name"
+          label="Required"
+          defaultValue="Name"
+          variant="filled"
+        />
+        <br />
+        <TextField
+          required
+          id="Email"
+          label="Required"
+          defaultValue="Email"
+          variant="filled"
+        />
+        <br />
+        <TextField
+          required
+          id="filled-required"
+          label="Required"
+          defaultValue="this needs to be a big text field"
+          variant="filled"
+        />
+        </Grid>
+        {/* need a submit button here */}
+        </form>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,6 @@ function Landing() {
       <div>
         <h1>Welcome</h1>
         <p style={{margin: 10}}>My name is Mark Ghaby</p>
-        <p>asdasd</p>
       </div>
     )
   }
