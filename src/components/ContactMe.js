2d1674a12e4391bca1c22214bc837be0e50c774d
mghaby <mark.ghaby71@hotmail.com>
2021-02-17 13:13:17 +1100

    more general site styling changes

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -48,6 +49,8 @@ function ContactMe() {
           defaultValue="this needs to be a big text field"
           variant="filled"
         />
+        <br/>
+        <Button variant="contained" color="default">Submit</Button>
         </Grid>
         {/* need a submit button here */}
         </form>
