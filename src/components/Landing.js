c3e4d0929c3d26ffddc00ec92cf8670f6090bb5c
mghaby <mark.ghaby71@hotmail.com>
2021-02-19 19:23:44 +1100

    data added to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,13 @@ function Landing() {
     return (
       <div>
         <h1>Welcome</h1>
-        <p style={{margin: 10}}>My name is Mark Ghaby</p>
+        <p style={{margin: 10}}>Hello! My name is Mark Ghaby. Im a Sydney-based Junior Full Stack Developer.</p>
+        {/* an img should be here of self */}
+        <br />
+        <p>My skills range from .... In the future I'd like to
+          diversify this list and potentially branch into Penetration 
+          Testing in the Cyber Security world. </p>
+          {/* a graphic of language list sshould be somewhere around here */}
         <hr />
       </div>
     )
