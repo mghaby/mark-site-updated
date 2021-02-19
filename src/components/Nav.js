ee128b1d17d66042cf750ec5b6d42db0c0766eee
mghaby <mark.ghaby71@hotmail.com>
2021-02-19 14:15:50 +1100

    nav bar is sticky, no onChange setup, nav also renders over landing by default

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,16 +7,44 @@ import Projects from './Projects';
 import AcaHistory from './AcaHistory';
 import ContactMe from './ContactMe';
 import App from '../App'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
 
 
 function Nav() {
+  const classes = useStyles();
+  // const [value, setValue] = React.useState(0);
+
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  // };
+
     return (
-      <div style={{backgroundColor: "#343A40", color: "white"}}>
-      <p>Home</p>
-      <p>Projects</p>
-      <p>Academic History</p>
-      <p>Contact Me</p>
-      <hr />
+      <div style={{position: "fixed", width:"100%"}}>
+      <Paper style={{backgroundColor: "#343A40", color: "white"}}>
+      <Tabs
+        // value={}
+        // onChange={}
+        indicatorColor="white"
+        textColor="white"
+        centered
+      >
+        <Tab label="Home" />
+        <Tab label="Projects" />
+        <Tab label="Academic History" />
+        <Tab label="Contact me" />
+      </Tabs>
+    </Paper>
+    <hr />
       </div>
     );
   }
