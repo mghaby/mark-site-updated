2f2df52cf4ccae965c7c22223d995eb79aee48b9
mghaby <mark.ghaby71@hotmail.com>
2021-03-03 13:41:52 +1100

    updates to project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,13 +16,18 @@ function Projects() {
 
         <div style={divCent}>
           <h3>This Website</h3>
-          <p>say stack / tech / why u made it</p>
+          <p>This web app builds off an old website made for an Assignment</p>
+          <p>The old website was my first ever code project in Vanilla HTML and CSS</p>
+          <p>This application is in React {`&`} JS </p>
+          {/* <p>https://github.com/mghaby/mark-site-updated</p> */}
           <br />
         </div>
 
         <div style={divCent}>
-          <h3>updated terminal app or a diff ruby app</h3>
-          <p>say stack / tech / why u made it</p>
+          <h3>Game Shop Ruby CLI Application</h3>
+          <p>This Ruby CLI App runs in terminal and simulates visiting a Game Store</p>
+          <p>Inspired by another Ruby CLI assignment, both in Ruby with an assortment of Gems</p>
+          {/* <p>https://github.com/mghaby/game_shop</p> */}
           <br />
         </div>
 
