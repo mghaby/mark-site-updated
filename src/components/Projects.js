6fa8ef2a220700d2ec028dfa7f35268315104b98
mghaby <mark.ghaby71@hotmail.com>
2021-03-05 13:16:25 +1100

    styling changes added to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,10 @@
+// import ruby from '../img/ruby'
+import react_project from '../img/react_project.png'
 
+const proImg = {
+  display: 'inline-block',
+  height: 262
+};
 
 const proText = {
   display: 'flex',
@@ -13,31 +19,36 @@ function Projects() {
     return (
       <div>
         <h1 style={{textAlign: "center"}}>Projects</h1>
-
+{/* make a card graphic thing for the projects  */}
         <div style={divCent}>
           <h3>This Website</h3>
           <p>This web app builds off an old website made for an Assignment</p>
           <p>The old website was my first ever code project in Vanilla HTML and CSS</p>
           <p>This application is in React {`&`} JS </p>
           {/* <p>https://github.com/mghaby/mark-site-updated</p> */}
-          <br />
         </div>
+        <br />
+        <br />
 
         <div style={divCent}>
           <h3>Game Shop Ruby CLI Application</h3>
+          {/* <img src={ruby} alt="CLI App Image" style={proImg}/> */}
           <p>This Ruby CLI App runs in terminal and simulates visiting a Game Store</p>
           <p>Inspired by another Ruby CLI assignment, both in Ruby with an assortment of Gems</p>
           {/* <p>https://github.com/mghaby/game_shop</p> */}
-          <br />
         </div>
+        <br />
+        <br />
 
         <div style={divCent}>
           <h3>React Full Stack App</h3>
-          <p>same as above</p>
-          <br />
+          <img src={react_project} alt="Full Stack Preview" style={proImg}/>
+          <p>The app was made to help people reach their fitness goals</p>
+          <p>The app was made with a Rails API and a React Frontend</p>
+          {/* <p>https://heartline-react.netlify.app/</p> */}
         </div>
-
-
+        <br />
+        <br />
 
         <hr />
       </div>
