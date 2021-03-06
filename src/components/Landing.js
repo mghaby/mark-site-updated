1cce679768add23f5cbe08af9122d639962de92a
mghaby <mark.ghaby71@hotmail.com>
2021-03-06 12:52:48 +1100

    changes to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,28 @@
+import me from '../img/me.png'
 
+const pfpStyle = {
+  display: "inline-block",
+  float: "right",
+  height: 150,
+  width: 150,
+  // marginRight: 250
+};
 
 function Landing() {
     return (
       <div>
         <h1>Welcome</h1>
-        <p style={{margin: 10}}>Hello! My name is Mark Ghaby. Im a Sydney-based Junior Full Stack Developer.</p>
-          {/* an img should be here of self */}
-        <br />
-        <p>My skills range from .... In the future I'd like to
+        <div style={{textAlign: "center", alignContent: "center", alignItems: "center"}}>
+        <p style={{display: "inline-block"}}>Hello! My name is Mark Ghaby. Im a Sydney-based Junior Full Stack Developer.</p>
+        <img src={me} alt={"Photo of Mark Ghaby"} style={pfpStyle} />
+        </div>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <p>My skills range from JavaScript, Ruby, React, Ruby on Rails and HTML {"&"} CSS.
+          In the future I'd like to
           diversify this list and potentially branch into Penetration 
           Testing in the Cyber Security world. </p>
           {/* a graphic of language list sshould be somewhere around here */}
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <hr />
       </div>
     )
